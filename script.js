042bb612a132e89c0d0badcb0260d3d1c13062fd
Wesley Slimani <wesley.slimani@gmail.com>
2023-02-28 14:06:14 +0100

    fix: made code cleaner and tweaked variable/function names

--- script.js
+++ script.js
@@ -25,65 +25,60 @@ const images = slides.map((slide) => slide.image);
 const text = slides.map((slide) => slide.tagLine);
 
 const bannerParagraph = document.getElementById("banner").children[1];
-const dots = document.querySelector(".dots");
-
-const htmlSlide = document
+const bannerImage = document
   .getElementById("banner")
   .getElementsByClassName("banner-img")[0];
+const dots = document.querySelector(".dots");
 
 let slideIndex = 0;
 
-for (let i = 0; i < slides.length; i++) {
+slides.forEach(() => {
   const dots = document.querySelector(".dots");
   const dotDiv = document.createElement("div");
   dotDiv.classList.add("dot");
   dots.appendChild(dotDiv);
   dots.children[0].classList.add("dot_selected");
-}
+});
 
-const nextDot = (slideIndex) => {
-  let nextDot = slideIndex + 1;
-
+const fillNextDot = (slideIndex) => {
   dots.children[slideIndex].classList.add("dot_selected");
   if (slideIndex == 3) {
     dots.children[0].classList.remove("dot_selected");
   } else {
-    dots.children[nextDot].classList.remove("dot_selected");
+    dots.children[slideIndex + 1].classList.remove("dot_selected");
   }
 };
-const previousDot = (slideIndex) => {
-  let prevDot = slideIndex - 1;
-
+const fillPreviousDot = (slideIndex) => {
   dots.children[slideIndex].classList.add("dot_selected");
   if (slideIndex == 0) {
     dots.children[slides.length - 1].classList.remove("dot_selected");
   } else {
-    dots.children[prevDot].classList.remove("dot_selected");
+    dots.children[slideIndex - 1].classList.remove("dot_selected");
   }
 };
 
-const prevSlide = () => {
+const toPrevSlide = () => {
   if (slideIndex <= 0) {
     slideIndex = slides.length;
   }
 
   slideIndex = slideIndex - 1;
 
-  nextDot(slideIndex);
-  htmlSlide.src = "./assets/images/slideshow/" + images[slideIndex];
+  fillNextDot(slideIndex);
+  bannerImage.src = "./assets/images/slideshow/" + images[slideIndex];
   bannerParagraph.innerHTML = text[slideIndex];
 };
 
-const nextSlide = () => {
+const toNextSlide = () => {
   slideIndex = slideIndex + 1;
-  let prevDot = slideIndex - 1;
+
   if (slideIndex >= slides.length) {
     slideIndex = 0;
   }
-  previousDot(slideIndex);
-  htmlSlide.src = "./assets/images/slideshow/" + images[slideIndex];
+  fillPreviousDot(slideIndex);
+  bannerImage.src = "./assets/images/slideshow/" + images[slideIndex];
   bannerParagraph.innerHTML = text[slideIndex];
 };
 
-arrowLeft.addEventListener("click", prevSlide);
-arrowRight.addEventListener("click", nextSlide);
+arrowLeft.addEventListener("click", toPrevSlide);
+arrowRight.addEventListener("click", toNextSlide);
